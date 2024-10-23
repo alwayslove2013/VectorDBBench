0a6c9ee8608cf82da7c9cb626fc226a13e1a7dbd
min.tian <min.tian.cn@gmail.com>
2024-10-23 09:48:28 +0800

    labels filter (like color==red)
    Signed-off-by: min.tian <min.tian.cn@gmail.com>

--- vectordb_bench/backend/runner/serial_runner.py
+++ vectordb_bench/backend/runner/serial_runner.py
@@ -9,6 +9,8 @@ import psutil
 import numpy as np
 import pandas as pd
 
+from vectordb_bench.backend.filter import Filter, LabelFilter
+
 from ..clients import api
 from ...metric import calc_ndcg, calc_recall, get_ideal_dcg
 from ...models import LoadTimeoutError, PerformanceTimeoutError
@@ -22,33 +24,58 @@ WAITTING_TIME = 60
 
 log = logging.getLogger(__name__)
 
+
 class SerialInsertRunner:
-    def __init__(self, db: api.VectorDB, dataset: DatasetManager, normalize: bool, timeout: float | None = None):
+    def __init__(
+        self,
+        db: api.VectorDB,
+        dataset: DatasetManager,
+        normalize: bool,
+        filter: Filter = None,
+        timeout: float | None = None,
+    ):
         self.timeout = timeout if isinstance(timeout, (int, float)) else None
         self.dataset = dataset
         self.db = db
         self.normalize = normalize
+        self.filter = filter
 
     def task(self) -> int:
         count = 0
         with self.db.init():
-            log.info(f"({mp.current_process().name:16}) Start inserting embeddings in batch {config.NUM_PER_BATCH}")
+            log.info(
+                f"({mp.current_process().name:16}) Start inserting embeddings in batch {config.NUM_PER_BATCH}"
+            )
             start = time.perf_counter()
             for data_df in self.dataset:
-                all_metadata = data_df['id'].tolist()
+                all_metadata = data_df["id"].tolist()
 
-                emb_np = np.stack(data_df['emb'])
+                emb_np = np.stack(data_df["emb"])
                 if self.normalize:
                     log.debug("normalize the 100k train data")
-                    all_embeddings = (emb_np / np.linalg.norm(emb_np, axis=1)[:, np.newaxis]).tolist()
+                    all_embeddings = (
+                        emb_np / np.linalg.norm(emb_np, axis=1)[:, np.newaxis]
+                    ).tolist()
                 else:
                     all_embeddings = emb_np.tolist()
-                del(emb_np)
-                log.debug(f"batch dataset size: {len(all_embeddings)}, {len(all_metadata)}")
+                del emb_np
+                log.debug(
+                    f"batch dataset size: {len(all_embeddings)}, {len(all_metadata)}"
+                )
+
+                labels_data = None
+                if isinstance(self.filter, LabelFilter):
+                    if self.dataset.data.scalar_labels_file_separated:
+                        labels_data = self.dataset.scalar_labels[
+                            self.filter.label_field
+                        ][all_metadata].to_list()
+                    else:
+                        labels_data = data_df[self.filter.label_field].tolist()
 
                 insert_count, error = self.db.insert_embeddings(
                     embeddings=all_embeddings,
                     metadata=all_metadata,
+                    labels_data=labels_data,
                 )
                 if error is not None:
                     raise error
@@ -56,30 +83,44 @@ class SerialInsertRunner:
                 assert insert_count == len(all_metadata)
                 count += insert_count
                 if count % 100_000 == 0:
-                    log.info(f"({mp.current_process().name:16}) Loaded {count} embeddings into VectorDB")
+                    log.info(
+                        f"({mp.current_process().name:16}) Loaded {count} embeddings into VectorDB"
+                    )
 
-            log.info(f"({mp.current_process().name:16}) Finish loading all dataset into VectorDB, dur={time.perf_counter()-start}")
+            log.info(
+                f"({mp.current_process().name:16}) Finish loading all dataset into VectorDB, dur={time.perf_counter()-start}"
+            )
             return count
 
-    def endless_insert_data(self, all_embeddings, all_metadata, left_id: int = 0) -> int:
+    def endless_insert_data(
+        self, all_embeddings, all_metadata, left_id: int = 0
+    ) -> int:
         with self.db.init():
             # unique id for endlessness insertion
-            all_metadata = [i+left_id for i in all_metadata]
+            all_metadata = [i + left_id for i in all_metadata]
 
-            NUM_BATCHES = math.ceil(len(all_embeddings)/NUM_PER_BATCH)
-            log.info(f"({mp.current_process().name:16}) Start inserting {len(all_embeddings)} embeddings in batch {NUM_PER_BATCH}")
+            NUM_BATCHES = math.ceil(len(all_embeddings) / NUM_PER_BATCH)
+            log.info(
+                f"({mp.current_process().name:16}) Start inserting {len(all_embeddings)} embeddings in batch {NUM_PER_BATCH}"
+            )
             count = 0
             for batch_id in range(NUM_BATCHES):
                 retry_count = 0
                 already_insert_count = 0
-                metadata = all_metadata[batch_id*NUM_PER_BATCH : (batch_id+1)*NUM_PER_BATCH]
-                embeddings = all_embeddings[batch_id*NUM_PER_BATCH : (batch_id+1)*NUM_PER_BATCH]
+                metadata = all_metadata[
+                    batch_id * NUM_PER_BATCH : (batch_id + 1) * NUM_PER_BATCH
+                ]
+                embeddings = all_embeddings[
+                    batch_id * NUM_PER_BATCH : (batch_id + 1) * NUM_PER_BATCH
+                ]
 
-                log.debug(f"({mp.current_process().name:16}) batch [{batch_id:3}/{NUM_BATCHES}], Start inserting {len(metadata)} embeddings")
+                log.debug(
+                    f"({mp.current_process().name:16}) batch [{batch_id:3}/{NUM_BATCHES}], Start inserting {len(metadata)} embeddings"
+                )
                 while retry_count < LOAD_MAX_TRY_COUNT:
                     insert_count, error = self.db.insert_embeddings(
-                        embeddings=embeddings[already_insert_count :],
-                        metadata=metadata[already_insert_count :],
+                        embeddings=embeddings[already_insert_count:],
+                        metadata=metadata[already_insert_count:],
                     )
                     already_insert_count += insert_count
                     if error is not None:
@@ -91,17 +132,23 @@ class SerialInsertRunner:
                             raise error
                     else:
                         break
-                log.debug(f"({mp.current_process().name:16}) batch [{batch_id:3}/{NUM_BATCHES}], Finish inserting {len(metadata)} embeddings")
+                log.debug(
+                    f"({mp.current_process().name:16}) batch [{batch_id:3}/{NUM_BATCHES}], Finish inserting {len(metadata)} embeddings"
+                )
 
                 assert already_insert_count == len(metadata)
                 count += already_insert_count
-            log.info(f"({mp.current_process().name:16}) Finish inserting {len(all_embeddings)} embeddings in batch {NUM_PER_BATCH}")
+            log.info(
+                f"({mp.current_process().name:16}) Finish inserting {len(all_embeddings)} embeddings in batch {NUM_PER_BATCH}"
+            )
         return count
 
     @utils.time_it
     def _insert_all_batches(self) -> int:
         """Performance case only"""
-        with concurrent.futures.ProcessPoolExecutor(mp_context=mp.get_context('spawn'), max_workers=1) as executor:
+        with concurrent.futures.ProcessPoolExecutor(
+            mp_context=mp.get_context("spawn"), max_workers=1
+        ) as executor:
             future = executor.submit(self.task)
             try:
                 count = future.result(timeout=self.timeout)
@@ -122,7 +169,10 @@ class SerialInsertRunner:
         # datasets for load tests are quite small, can fit into memory
         # only 1 file
         data_df = [data_df for data_df in self.dataset][0]
-        all_embeddings, all_metadata = np.stack(data_df["emb"]).tolist(), data_df['id'].tolist()
+        all_embeddings, all_metadata = (
+            np.stack(data_df["emb"]).tolist(),
+            data_df["id"].tolist(),
+        )
 
         start_time = time.perf_counter()
         max_load_count, times = 0, 0
@@ -130,12 +180,18 @@ class SerialInsertRunner:
             with self.db.init():
                 self.db.ready_to_load()
             while time.perf_counter() - start_time < self.timeout:
-                count = self.endless_insert_data(all_embeddings, all_metadata, left_id=max_load_count)
+                count = self.endless_insert_data(
+                    all_embeddings, all_metadata, left_id=max_load_count
+                )
                 max_load_count += count
                 times += 1
-                log.info(f"Loaded {times} entire dataset, current max load counts={utils.numerize(max_load_count)}, {max_load_count}")
+                log.info(
+                    f"Loaded {times} entire dataset, current max load counts={utils.numerize(max_load_count)}, {max_load_count}"
+                )
         except Exception as e:
-            log.info(f"Capacity case load reach limit, insertion counts={utils.numerize(max_load_count)}, {max_load_count}, err={e}")
+            log.info(
+                f"Capacity case load reach limit, insertion counts={utils.numerize(max_load_count)}, {max_load_count}, err={e}"
+            )
             traceback.print_exc()
             return max_load_count
         else:
@@ -153,13 +209,13 @@ class SerialSearchRunner:
         self,
         db: api.VectorDB,
         test_data: list[list[float]],
-        ground_truth: pd.DataFrame,
+        ground_truth: list[list[int]],
         k: int = 100,
-        filters: dict | None = None,
+        filter: dict | None = None,
     ):
         self.db = db
         self.k = k
-        self.filters = filters
+        self.filter = filter
 
         if isinstance(test_data[0], np.ndarray):
             self.test_data = [query.tolist() for query in test_data]
@@ -168,23 +224,22 @@ class SerialSearchRunner:
         self.ground_truth = ground_truth
 
     def search(self, args: tuple[list, pd.DataFrame]):
-        log.info(f"{mp.current_process().name:14} start search the entire test_data to get recall and latency")
+        log.info(
+            f"{mp.current_process().name:14} start search the entire test_data to get recall and latency"
+        )
         with self.db.init():
+            self.db.prepare_filter(self.filter)
             test_data, ground_truth = args
             ideal_dcg = get_ideal_dcg(self.k)
 
             log.debug(f"test dataset size: {len(test_data)}")
-            log.debug(f"ground truth size: {ground_truth.columns}, shape: {ground_truth.shape}")
+            log.debug(f"ground truth size: {len(ground_truth)}")
 
             latencies, recalls, ndcgs = [], [], []
             for idx, emb in enumerate(test_data):
                 s = time.perf_counter()
                 try:
-                    results = self.db.search_embedding(
-                        emb,
-                        self.k,
-                        self.filters,
-                    )
+                    results = self.db.search_embedding(emb, self.k)
 
                 except Exception as e:
                     log.warning(f"VectorDB search_embedding error: {e}")
@@ -193,13 +248,14 @@ class SerialSearchRunner:
 
                 latencies.append(time.perf_counter() - s)
 
-                gt = ground_truth['neighbors_id'][idx]
-                recalls.append(calc_recall(self.k, gt[:self.k], results))
-                ndcgs.append(calc_ndcg(gt[:self.k], results, ideal_dcg))
-
+                gt = ground_truth[idx]
+                recalls.append(calc_recall(self.k, gt[: self.k], results))
+                ndcgs.append(calc_ndcg(gt[: self.k], results, ideal_dcg))
 
                 if len(latencies) % 100 == 0:
-                    log.debug(f"({mp.current_process().name:14}) search_count={len(latencies):3}, latest_latency={latencies[-1]}, latest recall={recalls[-1]}")
+                    log.debug(
+                        f"({mp.current_process().name:14}) search_count={len(latencies):3}, latest_latency={latencies[-1]}, latest recall={recalls[-1]}"
+                    )
 
         avg_latency = round(np.mean(latencies), 4)
         avg_recall = round(np.mean(recalls), 4)
@@ -214,9 +270,8 @@ class SerialSearchRunner:
             f"avg_ndcg={avg_ndcg},"
             f"avg_latency={avg_latency}, "
             f"p99={p99}"
-         )
+        )
         return (avg_recall, avg_ndcg, p99)
-
 
     def _run_in_subprocess(self) -> tuple[float, float]:
         with concurrent.futures.ProcessPoolExecutor(max_workers=1) as executor:
